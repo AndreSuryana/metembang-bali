4bbd0df87eefba217f7bd19ec16e11d9b780cd0c
AndreSuryana <Andresuryana17@gmail.com>
2022-12-16 19:29:20 +0800

    add: create user seeder for testing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,9 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // Call seeder
         $this->call([
-            AdminSeeder::class
+            AdminSeeder::class,
+            UserSeeder::class
         ]);
     }
 }
